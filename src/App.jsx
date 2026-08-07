4b14cda71c92da5d15d73f94a36e1ddd0b4adcbe
Rakhesh R <rrakhesh2005@gmail.com>
2026-08-07 11:36:41 +0530

    project structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h2 className='text-black-200 font-roboto text-xl'>MAIN component</h2>
+    </>
+  )
+}
+
+export default App
